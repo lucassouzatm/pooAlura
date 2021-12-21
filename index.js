51448df58956ccdde26a431a3013b8ff9af0684b
Lucas Souza <lucassouzatm@gmail.com>
2021-12-21 11:41:09 -0300

    Aula 03

--- index.js
+++ index.js
@@ -1,44 +1,25 @@
-class Cliente{
-    nome;
-    cpf;
-}
+import { Cliente } from "./Cliente.js";
+import { ContaCorrente } from "./ContaCorrente.js";
 
-class ContaCorrente{
-    agencia;
-    _saldo = 0;
-    
-    sacar(valor){
-        if(this._saldo >= valor){
-            this._saldo -= valor;
-            return valor
-        }
-    }
-
-    depositar(valor){
-        if(valor <= 0)
-        {
-            return
-        }
-        this._saldo += valor;
-    }
-}
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
 
+const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
 
 const contaCorrenteRicardo = new ContaCorrente();
 contaCorrenteRicardo.agencia = 1001;
+contaCorrenteRicardo.cliente = cliente1;
+contaCorrenteRicardo.depositar(500);
 
-contaCorrenteRicardo.depositar(-100);
-contaCorrenteRicardo.depositar(100);
-contaCorrenteRicardo.depositar(100);
+const conta2 = new ContaCorrente();
+conta2.cliente = cliente2
+conta2.agencia = 102;
 
-const valorSacado = contaCorrenteRicardo.sacar(50);
-console.log(valorSacado);
+let valor = 200;
+contaCorrenteRicardo.transferir(valor, conta2);
 
-console.log(contaCorrenteRicardo);
+console.log("valor", valor)
+console.log(conta2);
