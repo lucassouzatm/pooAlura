f61893e7d4658151b241661f8e844145db438ccb
Lucas Souza <lucassouzatm@gmail.com>
2021-12-26 22:07:23 -0300

    Aula 02 - IH

--- Conta.js
+++ Conta.js
@@ -1,22 +1,41 @@
-export class Conta{
-    constructor(saldoInicial, cliente, agencia){
+export class Conta {
+    constructor(saldoInicial, cliente, agencia) {
         this._saldo = saldoInicial;
         this._cliente = cliente;
         this._agencia = agencia;
+       
     }
 
-    sacar(valor){
-        taxa = 1.1 * valor;
-        if (this._saldo >= valor){
-            this._saldo -= valor;
-            return valor;
+    set cliente(novoValor) {
+        if (novoValor instanceof Cliente) {
+            this._cliente = novoValor;
         }
     }
 
-    depositar(valor){
-        if (valor <= 100) {
-            return;
+    get cliente() {
+        return this._cliente;
+    }
+
+    get saldo() {
+        return this._saldo;
+    }
+
+    sacar(valor) {
+        let taxa = 1
+        return this._sacar(valor, taxa);
+    }
+
+    _sacar(valor, taxa){
+        const valorSacado = taxa * valor;
+        if (this._saldo >= valorSacado) {
+            this._saldo -= valorSacado;
+            return valorSacado;
         }
+
+        return 0;
+    }
+
+    depositar(valor) {
         this._saldo += valor;
     }
 
